ac5aca30859a9fc5579ccf0d37331661fb620710
Arjun Munji <arjunmunji@gmail.com>
2021-08-08 19:50:10 +0530

    Update settings.py

--- linkinbio/app/settings.py
+++ linkinbio/app/settings.py
@@ -79,14 +79,6 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     },
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'linkinbio',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
 }
 
 
