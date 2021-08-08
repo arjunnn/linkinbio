86079e8d5a4baf0083f9b60a6512fc5502f9fd29
Arjun Munji <arjunmunji@gmail.com>
2021-08-08 18:37:16 +0530

    add fixtures for themes

--- linkinbio/app/settings.py
+++ linkinbio/app/settings.py
@@ -79,19 +79,6 @@ DATABASES = {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     },
-    "default": {
-        "NAME": "linkinbio",
-        "ENGINE": "mysql.connector.django",
-        "USER": "root",
-        "PASSWORD": "root",
-        "HOST": "localhost",
-        "PORT": "3306",
-        "OPTIONS": {
-            "autocommit": True,
-            "charset": "utf8mb4",
-            "use_unicode": True,
-        },
-    },
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'linkinbio',
